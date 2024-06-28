4cfcd53425d867b2c0180b55fbec37dc9e163b04
Osama <osamaalhyari99@gmail.com>
2024-06-29 00:48:14 +0300

    quiz front

---
+++ quiz/quiz.js
@@ -0,0 +1,7 @@
+// document.querySelector("body").onclick = () => {
+//   window.scrollBy({
+//     top: 0,
+//     left: window.innerWidth * 0.98,
+//     behavior: "smooth",
+//   });
+// }; // handle arrows and doubleclicks
