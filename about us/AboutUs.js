3292acc1104a94619c0875a621c8a23a2974da48
Osama <osamaalhyari99@gmail.com>
2024-07-01 19:05:09 +0300

    fixed about us connections

--- about us/AboutUs.js
+++ about us/AboutUs.js
@@ -1,20 +1,14 @@
-function handleLogoutClick()
-{
-    window.location.href =  "../register/register.html";
-}
-
-
 document.addEventListener("DOMContentLoaded", () => {
-
-if (sessionStorage.getItem("id") && sessionStorage.getItem("name")) {
-  document.querySelector("#loginLogout").textContent = "Log Out";
-  document.querySelector("#loginLogout").addEventListener("click", () => {
-    sessionStorage.clear();
-    location.reload();
-  });
-} else {
-  document.querySelector("#loginLogout").textContent = "Log In";
-  document.querySelector("#loginLogout").addEventListener("click", () => {
-    window.location = "../register/register.html";
-})}
+  if (sessionStorage.getItem("id") && sessionStorage.getItem("name")) {
+    document.querySelector("#signUpLogOut").textContent = "Log Out";
+    document.querySelector("#signUpLogOut").addEventListener("click", () => {
+      sessionStorage.clear();
+      location.reload();
+    });
+  } else {
+    document.querySelector("#signUpLogOut").textContent = "Sign Up";
+    document.querySelector("#signUpLogOut").addEventListener("click", () => {
+      window.location = "../register/register.html";
+    });
+  }
 });
